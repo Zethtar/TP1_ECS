507ca62bf5ad57a68f8c1dde95ef04720195f5a9
Anthony Sirois <Anthonysirois999@gmail.com>
2018-09-05 12:29:05 -0400

    Colision component online NB. None of the above push have been tested. Tread carefully

--- ECS_E1/CollisionComponent.cpp
+++ ECS_E1/CollisionComponent.cpp
@@ -1,12 +1,20 @@
-#include "Collision.h"
+#include "CollisionComponent.h"
 
+CollisionComponent::CollisionComponent(sf::Rect<float> boundingBox)
+{
+    this->boundingBox = boundingBox;
+}
 
-
-Collision::Collision()
+CollisionComponent::~CollisionComponent()
 {
 }
 
-
-Collision::~Collision()
+bool CollisionComponent::CollideWith(sf::Rect<float> otherBoundingBox)
 {
+    return boundingBox.intersects(otherBoundingBox);
+}
+
+sf::Rect<float> CollisionComponent::GetBoundingBox()
+{
+    return boundingBox;
 }
--- ECS_E1/CollisionComponent.h
+++ ECS_E1/CollisionComponent.h
@@ -1,8 +1,20 @@
-#pragma once
+#ifndef COLLISION_COMPONENT
+#define COLLISION_COMPONENT
+
+#include <SFML/Graphics.hpp>
+
 class CollisionComponent
 {
 public:
-    CollisionComponent();
+    CollisionComponent(sf::Rect<float> boundingBox);
     ~CollisionComponent();
+
+    bool CollideWith(sf::Rect<float> otherBoundingBox);
+
+    sf::Rect<float> GetBoundingBox();
+
+private:
+    sf::Rect<float> boundingBox;
 };
 
+#endif // !COLLISION_COMPONENT
