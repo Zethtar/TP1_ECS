dd1e6b27f9df3180749bbce309f7f6b3213e58c9
Anthony Sirois <Anthonysirois999@gmail.com>
2018-09-05 11:58:44 -0400

    Done movement component, Want to die lvl increasing

--- ECS_E1/MovementComponent.cpp
+++ ECS_E1/MovementComponent.cpp
@@ -1,12 +1,26 @@
-#include "Movement.h"
+#include "MovementComponent.h"
 
+MovementComponent::MovementComponent(sf::Vector2f initialPosition)
+{
+    position = initialPosition;
+}
 
-
-Movement::Movement()
+MovementComponent::~MovementComponent()
 {
 }
 
-
-Movement::~Movement()
+void MovementComponent::Move(float xOffset, float yOffset)
 {
+    position.x += xOffset;
+    position.y += yOffset;
+}
+
+void MovementComponent::Move(sf::Vector2f offset)
+{
+    Move(offset.x, offset.y);
+}
+
+sf::Vector2f MovementComponent::GetPosition()
+{
+    return position;
 }
--- ECS_E1/MovementComponent.h
+++ ECS_E1/MovementComponent.h
@@ -1,8 +1,21 @@
-#pragma once
+#ifndef MOVEMENT_COMPONENT
+#define MOVEMENT_COMPONENT
+
+#include <SFML/Graphics.hpp>
+
 class MovementComponent
 {
 public:
-    MovementComponent();
+    MovementComponent(sf::Vector2f initialPosition = sf::Vector2f());
     ~MovementComponent();
+
+    void Move(float xOffset, float yOffset);
+    void Move(sf::Vector2f offset);
+
+    sf::Vector2f GetPosition();
+
+private:
+    sf::Vector2f position;
 };
 
+#endif // !MOVEMENT_COMPONENT
