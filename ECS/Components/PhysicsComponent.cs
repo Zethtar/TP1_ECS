dc033d483e509cade2fe55932e052f80e0962eda
Anthony Sirois <Anthonysirois999@gmail.com>
2018-09-13 15:28:51 -0400

    Start ECS : Plz no brain dead

---
+++ ECS/Components/PhysicsComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+                                                                  
+namespace ECS.Components                                          
+{                                                                 
+    class PhysicsComponent                                        
+    {                                                             
+        public float xDirection;                                  
+        public float yDirection;                                  
+                                                                  
+        public float xPosition;                                   
+        public float yPosition;                                   
+    }                                                             
+}                                                                 
+                                                                  
---
+++ ECS/Components/RenderComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Components
+{
+    class RenderComponent
+    {
+        public char character;
+        public int xPos;
+        public int yPos;
+    }
+}
